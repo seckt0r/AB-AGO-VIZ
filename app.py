84cbc276978161b137740a58901ed739662f5089
seckt0r <benone.marcos@gmail.com>
2024-08-20 21:32:25 +0100

    Adicionado o menu e o sistema de filtros com no momento 3 opções.

--- app.py
+++ app.py
@@ -3,6 +3,12 @@ import pandas as pd
 import pyreadstat
 import plotly.graph_objects as go
 
+st.title("Visualização de Dados do Afrobarômetro")
+st.markdown("""
+Os dados a seguir são extraídos do conjunto de dados do Round 9 (2022) referentes a Angola, do Afrobarômetro.
+""")
+st.markdown("---")
+
 # Load the questionnaire data
 df, meta = pyreadstat.read_sav('afrobarometer_release-dataset_ang_r9_pt_2023-03-01.sav')
 
@@ -22,38 +28,96 @@ categories = list(question_descriptions.keys())
 st.sidebar.title('Selecione uma categoria')
 category = st.sidebar.radio('', categories)
 
-# Display all answer charts for questions inside the selected category
-st.header(category)
-for question_var, question_description in question_descriptions[category].items():
-    answer_counts = df[question_var].value_counts()
-    answer_labels = meta.variable_value_labels[question_var]
-    answer_counts.index = [answer_labels[i] for i in answer_counts.index]
-    st.subheader(question_description)
-    # st.bar_chart(answer_counts, x_label="Total de Respostas", y_label="Respostas", stack=False, horizontal=True)
-    # Create a Plotly bar chart
-    fig = go.Figure(data=[go.Bar(
-        x=answer_counts.values,
-        y=answer_counts.index,
-        orientation='h'  # Horizontal bars
-    )])
+# Define the columns for filtering (non-sequential)
+filter_columns = ['URBRUR', 'REGION', 'THISINT']
 
-    # Add labels to the top of each bar
-    for i in range(len(answer_counts)):
-        fig.add_annotation(
-            x=answer_counts.values[i],
-            y=answer_counts.index[i],
-            text=str(answer_counts.values[i]),
-            showarrow=False,
-            font=dict(size=10, color='black', weight='bold'),  # Black and bold text
-            yshift=0,  # Center the label on the bar
-            xanchor='center',  # Center the label horizontally
-            bgcolor='yellow',  # Yellow background
-            opacity=0.8  # Adjust background transparency
-        )
+# Create the filter options
+st.sidebar.title('Filtros')
+filter_values = {}
+for col in filter_columns:
+    # Get unique values and their descriptions
+    unique_values = df[col].unique()
+    value_labels = meta.variable_value_labels[col]
+    unique_descriptions = [value_labels[i] for i in unique_values]
+    
+    # Replace column name with custom text
+    custom_text = {
+        'URBRUR': 'Contexto',
+        'REGION': 'Província',
+        'THISINT': 'Gênero'
+    }.get(col, col)  # Use custom text if available, otherwise use column name
 
-    fig.update_layout(
-#        title=question_description,
-        xaxis_title="Total de Respostas",
-        yaxis_title="Respostas"
-    )
-    st.plotly_chart(fig)
+    filter_values[col] = st.sidebar.multiselect(f'Selecione valores para {custom_text}', unique_descriptions, placeholder="Escolha uma opção")
+
+# Filter the data based on selected values
+filtered_df = df.copy()
+for col, values in filter_values.items():
+    if values:
+        # Get the value labels for the current column
+        value_labels = meta.variable_value_labels[col] 
+        # Get the corresponding indices for the selected descriptions
+        selected_indices = [list(value_labels.keys())[i] for i, desc in enumerate(value_labels.values()) if desc in values]
+        # Apply the filter correctly using the indices
+        filtered_df = filtered_df[filtered_df[col].isin(selected_indices)]
+
+# Check if any data remains after filtering
+if filtered_df.empty:
+    st.header("Nenhuma resposta corresponde aos filtros.")
+else:
+    # Display all answer charts for questions inside the selected category
+    st.header(category)
+    st.markdown("---")
+    for question_var in df.columns[2:]:  # Start from column Q3 (index 2)
+        if question_var in question_descriptions[category]:
+            question_description = question_descriptions[category][question_var]
+            answer_counts = filtered_df[question_var].value_counts()
+            answer_labels = meta.variable_value_labels[question_var]
+            answer_counts.index = [answer_labels[i] for i in answer_counts.index]
+            st.subheader(question_description)
+            # Create a Plotly bar chart
+            fig = go.Figure(data=[go.Bar(
+                x=answer_counts.values,
+                y=answer_counts.index,
+                orientation='h'  # Horizontal bars
+            )])
+
+            # Add labels to the top of each bar
+            for i in range(len(answer_counts)):
+                # Calculate percentage
+                percentage = (answer_counts.values[i] / answer_counts.sum()) * 100
+                # Format percentage to two decimal places
+                percentage_str = f"{percentage:.2f}%"
+                fig.add_annotation(
+                    x=answer_counts.values[i],
+                    y=answer_counts.index[i],
+                    text=f"{answer_counts.values[i]} ({percentage_str})",  # Add percentage to label
+                    showarrow=False,
+                    font=dict(size=10, color='black', weight='bold'),  # Black and bold text
+                    yshift=0,  # Center the label on the bar
+                    xanchor='center',  # Center the label horizontally
+                    bgcolor='yellow',  # Yellow background
+                    opacity=0.8  # Adjust background transparency
+                )
+
+            legend_text = []
+            for col, values in filter_values.items():
+                if values:
+                    legend_text.append(f"{col}: {', '.join(values)}")
+            fig.update_layout(
+                xaxis_title="Total de Respostas",
+                yaxis_title="Respostas",
+                legend_title="Filtros activos",
+                legend=dict(
+                    traceorder="normal",
+                    font=dict(
+                        family="sans-serif",
+                        size=12,
+                        color="black"
+                    ),
+                    bgcolor="LightSteelBlue",
+                    bordercolor="Black",
+                    borderwidth=2
+                )
+            )
+            st.plotly_chart(fig)
+            st.markdown("---")
---
+++ pages/sobre.py
@@ -0,0 +1,34 @@
+import streamlit as st
+
+st.title("Sobre a Visualização de Dados do Afrobarômetro")
+
+st.markdown("""
+Este projeto visa fornecer uma plataforma interativa e amigável para explorar e visualizar dados das pesquisas do [Afrobarômetro](https://www.afrobarometer.org/), referentes à Angola. 
+O Afrobarômetro é um projecto de pesquisa pan-africano que realiza pesquisas de opinião pública em vários países africanos, cobrindo uma ampla gama de tópicos relacionados à governança, democracia e desenvolvimento.
+
+**Objetivos:**
+
+* **Tornar os dados do Afrobarômetro referentes à Angola mais acessíveis e compreensíveis:** Buscamos tornar os dados complexos coletados pelo Afrobarômetro mais acessíveis a um público mais amplo, incluindo pesquisadores, formuladores de políticas, jornalistas e o público em geral.
+* **Permitir a exploração e análise de dados:** Nossa plataforma fornece ferramentas para explorar os dados de diferentes maneiras, permitindo que os usuários identifiquem tendências, padrões e insights.
+* **Promover a tomada de decisões informadas:** Ao fornecer acesso fácil a dados e visualizações, esperamos contribuir para uma tomada de decisões mais informada em Angola.
+
+**Autores:**
+
+Este projeto é desenvolvido e mantido por Benone Marcos.
+
+**Como contribuir:**
+
+Aguardamos contribuições de qualquer pessoa interessada em melhorar este projecto. Aqui estão algumas maneiras de contribuir:
+
+* **Relatar problemas:** Se você encontrar algum bug ou tiver sugestões de melhorias, reporte-os em nosso [repositório GitHub](https://github.com/seckt0r/AB-AGO-VIZ).
+* **Enviar solicitações de pull:** Se você tiver alterações de código ou novos recursos para adicionar, sinta-se à vontade para enviar uma solicitação de pull em nosso repositório GitHub.
+* **Fornecer feedback:** Seu feedback é valioso para nós. Compartilhe seus pensamentos e sugestões sobre como podemos melhorar o projecto.
+
+**Outras informações relevantes:**
+
+* **Fonte de dados:** Os dados usados neste projecto estão disponíveis publicamente no site do Afrobarômetro.
+* **Licença:** Este projecto é licenciado sob a [Licença MIT](https://opensource.org/licenses/MIT).
+* **Contacto:** Para quaisquer dúvidas ou solicitações, entre em contacto com benone.marcos@gmail.com.
+
+Esperamos que este projecto seja um recurso valioso para qualquer pessoa interessada em entender a dinâmica da  sociedade Angolana.
+""")
